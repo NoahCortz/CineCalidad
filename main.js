016eeca2c6845d8e4a5d2b7c87b9d5f8e497b9b1
Noah Dev <developerncz@gmail.com>
2023-08-23 21:40:11 -0600

    add: obtenemos una pelicula y la renderizamos en la vista

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-fetch("https://api.themoviedb.org/3/discover/movie?api_key=" + api_key)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+let miPelicula = "";
+
+// Callback
+// Promesas
+// Async Await
+
+// Función anónima
+const setearVariable = function (data) {
+    miPelicula = data;
+    let template = `
+        <h3>${ data.title }</h3>
+        <p>${ data.overview }</p>
+    `;
+
+    document.querySelector("#miPelicula").innerHTML = template;
+}
+
+// Funcion declarada | nombrada
+function obtenerPelicula() {
+    miPelicula = fetch("https://api.themoviedb.org/3/movie/12?api_key=" + api_key)
+        .then((res) => res.json())
+        .then((data) => setearVariable(data))
+        .catch(() => []);
+}
+
+// Función Arrow 'Flecha' ES6
+const miFuncionFlecha = () => 2 * 2;
+
+
+obtenerPelicula();
